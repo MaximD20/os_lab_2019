149e766e1ff935e51a5c9abe196954a16300c240
maximd20 <8180647@edu.miet.ru>
2020-03-06 20:06:58 +0000

    Udate average.sh in lab1 and Add Makefile in lab2

--- lab1/src/average.sh
+++ lab1/src/average.sh
@@ -1,11 +1,20 @@
 t=0
 count=0
+if (($#>1)); then
+for param in $@
+do
+t=$[t+param]
+count=$[count+1]
+done
+result=$[t/count]
+else
 for param in $(cat $1)
 do
 t=$[t+param]
 count=$[count+1]
 done
 result=$[t/count]
+fi
 echo "Количество чисел: $count"
 echo "-----------------"
 echo "Среднее арифметическое: $result"
