1ae18921a604c3cbf450a2fd4909c07cf8857a26
maximd20 <8180647@edu.miet.ru>
2020-03-18 19:11:01 +0000

    Update Project GetMinMax for Task1

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -1,12 +1,27 @@
 #include "find_min_max.h"
-
+#include <stdio.h>
 #include <limits.h>
 
 struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
+while(*array)
+{
+    printf("Элемент: %i\n",*array);
+    if(*array >min_max.max)
+    {
 
+        min_max.max = *array;
+        printf("Поменялось значение максимума. Текущее значение: %i\n",*array);
+    }
+    else if(*array < min_max.min)
+    {
+        min_max.min = *array;
+        printf("Поменялось значение минимума. Текущее значение: %i\n",*array);
+    }
+    array++;
+}
   // your code here
   return min_max;
 }
--- lab3/src/utils.c
+++ lab3/src/utils.c
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 void GenerateArray(int *array, unsigned int array_size, unsigned int seed) {
   srand(seed);
-  for (int i = 0; i < array_size; i++) {
+  int i;
+  for ( i = 0; i < array_size; i++) {
     array[i] = rand();
+    printf("Сгенерированный элемент %d\n", array[i]);
   }
 }
