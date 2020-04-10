9609cbac20d940bd43ecf5dd61f1f0fe5f0c87d2
MaximD20 <61091144+MaximD20@users.noreply.github.com>
2020-04-10 21:22:07 +0300

    Update and rename lab3/src/utils.c to  lab3/ src/utils.c

--- lab3/ src/utils.c
+++ lab3/ src/utils.c
@@ -7,6 +7,7 @@ void GenerateArray(int *array, unsigned int array_size, unsigned int seed) {
   int i;
   for ( i = 0; i < array_size; i++) {
     array[i] = rand();
-    printf("Сгенерированный элемент %d\n", array[i]);
   }
 }
+
+
