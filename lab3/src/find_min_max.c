c71a30b8a93a3f1f1dfe75048786cce99a9ad347
maximd20 <8180647@edu.miet.ru>
2020-04-10 12:22:08 +0000

    bugs fixed

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -6,19 +6,28 @@ struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
-while(*array)
+  int j,k=0;
+ // printf("Просматриваемый промежуток: от %i do %i", begin,end);
+for(j=begin;j<end;j++)
 {
+    if(j!=0 && j==begin)
+    {
+        for(k =0; k<begin;k++)
+        {
+            array++;
+        }
+    }
     printf("Элемент: %i\n",*array);
     if(*array >min_max.max)
     {
 
         min_max.max = *array;
-        printf("Поменялось значение максимума. Текущее значение: %i\n",*array);
+       // printf("Поменялось значение максимума. Текущее значение: %i\n",*array);
     }
     else if(*array < min_max.min)
     {
         min_max.min = *array;
-        printf("Поменялось значение минимума. Текущее значение: %i\n",*array);
+        //printf("Поменялось значение минимума. Текущее значение: %i\n",*array);
     }
     array++;
 }
